afaa116547eca9c3cc8ad4e8f80eca911bce2a7b
Ancishan <shantodev027@gmail.com>
2024-06-02 12:13:58 +0600

    [DONE] jwt and cookie parser

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const app = express()
 require('dotenv').config()
 const cors = require('cors')
 // const nodemailer = require('nodemailer')
-// const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
-// const jwt = require('jsonwebtoken')
-// const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+const jwt = require('jsonwebtoken')
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 const port = process.env.PORT || 8000
 // const { Resend } = require('resend')
 // const resend = new Resend(api_key)
@@ -19,9 +19,24 @@ const corsOptions = {
 app.use(cors(corsOptions))
 
 app.use(express.json())
-// app.use(cookieParser())
+app.use(cookieParser())
 
-
+// Verify Token Middleware
+const verifyToken = async (req, res, next) => {
+  const token = req.cookies?.token
+  console.log(token)
+  if (!token) {
+    return res.status(401).send({ message: 'unauthorized access' })
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      console.log(err)
+      return res.status(401).send({ message: 'unauthorized access' })
+    }
+    req.user = decoded
+    next()
+  })
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nhcslav.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -38,7 +53,36 @@ async function run() {
     const db = client.db('microtaskearning')
     const usersCollection = db.collection('users')
   
-   
+       // auth related api
+       app.post('/jwt', async (req, res) => {
+        const user = req.body
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+          expiresIn: '365d',
+        })
+        res
+          .cookie('token', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+          })
+          .send({ success: true })
+      })
+
+       // Logout
+    app.get('/logout', async (req, res) => {
+      try {
+        res
+          .clearCookie('token', {
+            maxAge: 0,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+          })
+          .send({ success: true })
+        console.log('Logout successful')
+      } catch (err) {
+        res.status(500).send(err)
+      }
+    })
 
         
 
@@ -54,10 +98,10 @@ async function run() {
 run().catch(console.dir)
 
 app.get('/', (req, res) => {
-  res.send('Hello from StayVista Server..')
+  res.send('Hello from micro task Server..')
 })
 
 
 app.listen(port, () => {
-  console.log(`StayVista is running on port ${port}`)
+  console.log(`micro task is running on port ${port}`)
 })
